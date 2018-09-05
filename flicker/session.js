802985ad6f3503f1e4f6dd7be8d4f6e2d4cbb4de
conqueror23 <wayneborong@gmail.com>
2018-09-05 18:49:04 +1000

    login page

--- flicker/session.js
+++ flicker/session.js
@@ -1,1 +1,29 @@
-var 
+var connect = require('connect')
+, RedisSessions = require('connect-redis');
+rs = new RedisSessions();
+
+rsapp = 'myapp';
+
+rs.create({
+	app : myapp,
+	id : 'bw1',
+	up : '127.0.0.1',
+	ttl: 3600,
+
+	d :{
+		kid : 'ill',
+		numbers : 10
+	}
+},
+function (err,resp){
+	console.log(resp);
+}
+)
+
+
+
+//server.use(connect.session({store : new RedisStore,secret: 'my secret'}));
+//server.use(connect.session());
+//console.log('sessions trying things');
+
+server.listen(3000);

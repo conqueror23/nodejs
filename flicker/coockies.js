46d90f66dd811320f768228064074286db84619f
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2018-09-05 11:26:17 +1000

    complete login

--- flicker/coockies.js
+++ flicker/coockies.js
@@ -1,12 +1,16 @@
+// modules
+var connect = require('connect')
+,users = require('users');
 
-var connect = require('connect');
+//create server
 
 var server = connect(
 	connect.logger('dev')
+	,connect.bodyParser()
 	,connect.cookieParser()
 	,connect.session({secret : 'my secret'})
 	,function (req,res,next){
-		if('/'==req.url &&req.session.logged_in){
+		if('/' == req.url && req.session.logged_in){
 			res.writeHead(200,{'Content-Type':'text/html'});
 			res.end(
 				'Welcome back, <b>' +req.session.name +'</b>.'
@@ -15,6 +19,45 @@ var server = connect(
 		}else{
 			
 			next();
+		}
+	}
+	,function (req,res,next){
+		if('/' == req.url && 'GET' == req.method ){
+			res.writeHead(200,{'Content-Type': 'text/html'});
+			res.end([
+			'<form action = "/login" method = "POST">'
+			,'<fieldset>'
+			,	'<legend>Please log in</legend>'
+			,	'<p>User: <input type ="text" name = "user"></p>'
+			,	'<p>Password :<input type = "password" name ="password"></p>'
+			,'</fieldset>'
+			,'</form>'
+			].join(''));
+		}else{
+			next();
+		}
+	}
+	,function (req,res,next){
+		if('/login' == req.url && 'POST' == req.method){
+			res.writeHead(200);
+			if(!users[req.body.user]||req.body.password!= users[req.body.user].password){
+				res.end('Username /password wrong');
+			}else{
+				req.session.logged_in = true;
+				res.session.name = users[req.body.user].name;
+				res.end('Authenticaed!');
+			}
+		}else{
+		next();
+		}
+	}
+	,function (req,res,next){
+	if('/logout' == req.url){
+		req.session.logged_in = false;
+		res.writeHead(200);
+		res.end('logged out');
+		}else{
+		next();
 		}
 	}
 	);

5af0465959ba8c1449a1ebe2a4e28b487ae226c0
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2018-09-14 01:55:18 +1000

    a6_final

--- a6/a6.js
+++ a6/a6.js
@@ -1,30 +1,88 @@
-//var fs = require('fs);
-
-
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var express = require('express');
-var app = express ();
-var http = require('http');
-var server = http.createServer(app);
+var fs = require("fs");
+var app = express();
 
-server(
-function(req,res,next){
-	res.writeHead(200,{'Content-type':'text/html'});
-	res.end('Hi there this is new format of this server ways');
-}
-);
+app.use(express.static('./public'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var vg = app.set('view engine','ejs');
+app.use(session({
+    secret:'secret' ,
+    name: 'newapp',
+    cookie: {maxAge: 80000 },
+    resave: false,
+    saveUninitialized: true,
+}));
+app.set("view engine", "ejs");
 
-app.set('views',__dirname +'/views');
+app.get('/', function (req, res) {
+  console.log("/ GET");
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' });
+  res.end([
+    '<form method = "post"><br/>Part 1:<br/>'
+    ,'<input name = "part1"><br/>Part 2:<br/><input name = "part2"><br/><button>submit</button></form>'
+    ,'<a href ="http://localhost:3000/send">send</a><br/>'
+    ,'<a href ="http://localhost:3000/json">json</a><br/>'
+    ,'<a href ="http://localhost:3000/redirect">redirect</a><br/>'
+    ,'<a href ="http://localhost:3000/sendfile">sendfile</a><br/>'
+    ,'<a href ="http://localhost:3000/googleapi">googleapi</a><br/>'
+    ,'<a href ="http://localhost:3000/render">render</a><br/>'
+  ].join(''));
+})
 
-app.set('view options', {layout : false});
+app.post('/', function (req, res) {
+  res.cookie("userid",'bw');
+  req.session.part1 = req.body.part1;
+  req.session.part2 = req.body.part2;
+  req.session.search = req.body.img;
+  res.redirect('http://chart.apis.google.com/chart?chs=400x400&chdlp=b&chtt=PiesOfRatios&chdl=part1|part2&chd=t:'+req.body.part1+','+req.body.part2+'&cht=p')
 
-console.log(app.set('views'));
+});
 
-console.log(app.set('view options'));
+app.get('/send', function(req, res) {
+  res.send('send request has been send to server');
+});
 
-//console.log(JSON.parse(vg));
+app.get('/json', function(req, res) {
+  res.json({user:'blong'});
+});
 
-app.get('/',function (req,res,next){});
+app.get('/redirect', function(req, res) {
+  res.redirect('/send');
+});
+
+ app.get('/googleapi', function(req, res) {
+  res.redirect('http://chart.apis.google.com/chart?chs=400x400&chdlp=b&chtt=ClockExample&chdl=SHOW|HIDE&chd=t:5,18&cht=c',301);
+ });
+
+app.get('/sendfile', function(req, res) {
+  // res.sendfile('1.jpg');
+  res.sendfile('1.txt');
+});
+
+app.get('/render', function(req, res) {
+  res.render('index');
+});
+
+
+// app.get('/googleapi', function(req, res) {
+//   //res.render('index.html');
+//   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' });
+//   // the functio here is just to read a file from local system
+//   require('fs').readFile('1.txt', function (err,data) {
+//   res.end(data);
+//   });
+// });
 
 app.listen(3000);
+//
+// var server = app.listen(3000, function () {
+//   var host = server.address().address
+//   var port = server.address().port
+//   console.log("http://%s:%s", host, port)
+//
+// })

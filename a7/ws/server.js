e8938fa91b3a43439f810a8d56165e517f84a1ba
conqueror23 <wayneborong@gmail.com>
2018-09-28 08:37:15 +1000

    websocket

---
+++ a7/ws/server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+, wsio = require('websocket.io');
+
+var http = require('http');
+
+var server = http.createServer(app);
+
+var app = express();
+
+var ws = wsio.attach(server);
+
+// var postion  = {}
+// , total = 0;
+
+server.use('/',express.static('public'));
+
+ws.on('connection',function (socket){
+  socket.on('message',function(msg){
+    console.log(msg);
+  })
+  socket.send('pong');
+});
+server.listen(3000);

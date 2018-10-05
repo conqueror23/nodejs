0a3b44fde7e4459aa114f255dc5e757bf0ab0682
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2018-10-05 16:17:21 +1000

    cursor-unfinished

--- a7/ws/server.js
+++ a7/ws/server.js
@@ -11,16 +11,43 @@ var server = http.createServer(app);
 var ws = wsio.attach(server);
 // than server
 
-// var postion  = {}
-// , total = 0;
+var positions  = {}
+, total = 0;
 
 app.use(express.static('public'));
 
 ws.on('connection',function (socket){
+  //cursors version
+  socket.id = ++total;
+  socket.send(JSON.stringify(positions));
 
   socket.on('message',function(msg){
-    console.log(msg);
-  })
-  socket.send('pong');
+    try{
+      var pos = JSON.parse(msg);
+
+    }catch(e){
+      console.log(e);
+      return ;
+    }
+    positions[socket.id] = pos;
+    // console.log(msg);
+    broadcast(JSON.stringify({type:'position',pos:pos,id :socket.id}));
+  });
+  socket.on('close',function (){
+    delete positions[socket.id];
+    broadcast(JSON.stringify({type:'disconnect',id :socket.id}));
+  });
+  function broadcast (msg){
+    for (var i = 0,l =ws.clients.length;i<l;i++){
+      if (ws.clients[i]&&socket.id !=ws.clietns[i].id){
+        ws.clients[i].send(msg);
+      }
+    }
+  }
+
+
+
+
+  // socket.send('pong');
 });
 server.listen(3000);

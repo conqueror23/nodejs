59e5c6065766e067907b68c5de0cb8b6c572a077
conqueror23 <wayneborong@gmail.com>
2018-10-07 00:05:19 +1000

    connect to mongo

--- a7/mongodb/app.js
+++ a7/mongodb/app.js
@@ -1,6 +1,22 @@
 var express = require('express');
 
 var mongodb = require('mongodb');
+// connectint to mongodb
+var server = new mongodb.Server('127.0.0.1',27017);
+new mongodb.Db('customer',server).open(function(err,client){
+if(err) throw err;
+console.log('connected to database');
+
+app.users= new mongodb.Collection (client,'users');
+
+// Collection.insert({my:'document'},function (err,docs){
+// 	if(err) throw err;
+// 	//db.close();
+//
+// });
+
+});
+
 
 var app = express.createServer();
 
@@ -21,8 +37,8 @@ app.get('/',function(req,res){
 	res.render('index',{authenticated:false});
 })
 
-app.get('/login',function (req,res){
-	res.render('login');
+app.get('/login/:signupEmail',function (req,res){
+	res.render('login',{signupEmail:req.params.signupEmail});
 })
 
 app.get('/signup',function(req,res){
@@ -30,6 +46,13 @@ app.get('/signup',function(req,res){
 
 })
 
+
+app.post('/signup',function (req,res,next){
+	app.users.insert(req.body.user,function(err,doc){
+		if(err) return next(err);
+		res.redirect('/login/'+doc[0].Email);
+	});
+});
 
 
 
